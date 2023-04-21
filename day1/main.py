c17ef9886920cc2d240e528d6e8a30916a930e49
OverLordYOLO <matej.kohutek@gmail.com>
2023-04-22 00:05:13 +0200

    part_1

---
+++ day1/main.py
@@ -0,0 +1,43 @@
+
+def loadLines():
+    lines = [0,0]
+    while not (lines[-1] == "" and lines[-2] == ""):
+        line = input()
+        if line != "":
+            line = int(line)
+        lines.append(line)
+    return lines[2:-1]
+
+def parseLines(lines):
+    elfs = []
+    elf = []
+    sum = 0
+    for line in lines:
+        if line == "":
+            elfs.append([sum, elf])
+            sum = 0
+        else:
+            elf.append(line)
+            sum += line
+    return elfs
+
+def findMaxElfIndex(elfs):
+    maxIndex = []
+    maxVal = 0
+    for i, elf in enumerate(elfs):
+        if elf[0] > maxVal:
+            maxIndex = i
+            maxVal = elf[0]
+
+    return maxIndex
+
+def main():
+    lines = loadLines()
+    elfs = parseLines(lines)
+    maxElfIndex = findMaxElfIndex(elfs)
+    print(f"elf n#:{maxElfIndex+1}; maxValue: {elfs[maxElfIndex][0]}")
+    
+
+
+if __name__ == "__main__":
+    main()
